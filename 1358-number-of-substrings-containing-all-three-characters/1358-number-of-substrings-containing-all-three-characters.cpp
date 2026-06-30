3ae55abe1337da07d699608f74815fa5680756ea
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-06-30 15:29:47 +0530

    Time: 8 ms (54.98%), Space: 11.1 MB (33.33%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,39 +2,61 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.length();
-        int target = 9;
-        int sum = 0;
-        vector<int> arr;
-        for(int i =0; i<n; ++i){
-            if(s[i]=='a'){
-                arr.push_back(1); // 1 for a
-            }
-            else if(s[i]=='b'){
-                arr.push_back(3); // 3 for b
-            }
-            else{
-                arr.push_back(5);   // 5 for c
-            }
-        }
-        bool hit = false;
+        vector<int> freq(3, 0);
+
+        int left = 0;
         int cnt = 0;
-        for(int i=0; i<n; ++i){
-            sum = arr[i];
-            hit = false;
-            for(int j=i; j<n; ++j){
-                    if(sum!=arr[j]){
-                    if(sum == 1 || sum == 3 || sum == 5){
-                        sum += arr[j];
-                    }
-                    else if(sum + arr[j] == target){
-                        sum += arr[j];
-                        hit =  true;
-                        cnt += n-j;
-                        break;
-                    }
-                   }
+
+        for (int right = 0; right < n; right++) {
+            freq[s[right] - 'a']++;
+
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                cnt += n - right;
+                freq[s[left] - 'a']--;
+                left++;
             }
         }
+
         return cnt;
     }
 };
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.length();
+//         int target = 9;
+//         int sum = 0;
+//         vector<int> arr;
+//         for(int i =0; i<n; ++i){
+//             if(s[i]=='a'){
+//                 arr.push_back(1); // 1 for a
+//             }
+//             else if(s[i]=='b'){
+//                 arr.push_back(3); // 3 for b
+//             }
+//             else{
+//                 arr.push_back(5);   // 5 for c
+//             }
+//         }
+//         bool hit = false;
+//         int cnt = 0;
+//         for(int i=0; i<n; ++i){
+//             sum = arr[i];
+//             hit = false;
+//             for(int j=i; j<n; ++j){
+//                     if(sum!=arr[j]){
+//                     if(sum == 1 || sum == 3 || sum == 5){
+//                         sum += arr[j];
+//                     }
+//                     else if(sum + arr[j] == target){
+//                         sum += arr[j];
+//                         hit =  true;
+//                         cnt += n-j;
+//                         break;
+//                     }
+//                    }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
