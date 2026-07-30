632f035cf0cd7c18c5837a5b2b63b3f3d035d1d0
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-07-30 17:54:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (23.66%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n =  word.length();
+        vector<int> freq(26, 0);
+        for (char ch : word) {
+            freq[ch - 'a']++;
+        }
+        int alpha = 1;
+        int sum = 0;
+        for(auto it: freq){
+            if(alpha<9){
+                if(it != 0){
+                    sum += 1*it;
+                    alpha++;
+                }
+            }
+            else if(alpha >= 9 && alpha <17){
+                if(it != 0){
+                    sum += 2*it;
+                    alpha++;
+                }
+            }
+            else if(alpha >= 17 && alpha < 25){
+                if(it != 0){
+                    sum += 3*it;
+                    alpha++;
+                }
+            }
+            else{
+                if(it != 0){
+                    sum += 4*it;
+                    alpha++;
+                }
+            } 
+        }
+        return sum;
+    }
+};
