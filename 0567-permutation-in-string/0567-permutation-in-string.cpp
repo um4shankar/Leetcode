9a22b55d8fce51432a611c34ef4f1ce6d1c4f8ba
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-08-09 13:44:36 +0530

    Time: 0 ms (100%), Space: 10 MB (70.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,15 +3,12 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-
-        if (n1 > n2)
+        if (n1 > n2){
             return false;
-
+        } 
         bool flag = false;
-
         vector<int> freq1(26, 0);
         vector<int> freq2(26, 0);
-
         for (int i = 0; i < n1; ++i) {
             freq1[s1[i] - 'a']++;
             freq2[s2[i] - 'a']++;
@@ -28,9 +25,9 @@ public:
             }
         }
 
-        if (freq1 == freq2)
+        if (freq1 == freq2){
             flag = true;
-
+        }
         return flag;
     }
 };
