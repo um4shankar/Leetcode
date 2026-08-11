aa0e779d4b4f9c2341e9efe7d6be06e83140cbb6
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-08-11 19:17:08 +0530

    Time: 114 ms (41.33%), Space: 14.4 MB (61.81%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    int lis(int i, vector<int>& nums) {
+        if (dp[i] != -1)
+            return dp[i];
+
+        int ans = 1;
+        for (int j = 0; j < i; ++j) {
+            if (nums[j] < nums[i]) {
+                ans = max(ans, lis(j, nums) + 1);
+            }
+        }
+        return dp[i] = ans;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int N = nums.size();
+        dp.assign(N, -1);
+        int ans = 0;
+        for (int i = 0; i < N; ++i) {
+            ans = max(ans, lis(i, nums));
+        }
+        return ans;
+    }
+};
