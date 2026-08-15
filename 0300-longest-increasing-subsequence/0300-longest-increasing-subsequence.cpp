ce26008cef9be4abddebbd37fa4162d45a7a3c89
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-08-16 01:10:42 +0530

    Time: 131 ms (40.07%), Space: 14.2 MB (77.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> dp;
-
-    int lis(int i, vector<int>& nums) {
-        if (dp[i] != -1) return dp[i];
-        int ans = 1;
-        for (int j = 0; j < i; ++j) {
-            if (nums[j] < nums[i]) {
-                ans = max(ans, lis(j, nums) + 1);
-            }
+    int func(int i, vector<int>& nums){
+      if(i<0) return 0;
+      if(dp[i]!= -1) return dp[i];
+      dp[i] = 1;
+      for(int j=i-1; j>=0; --j){
+        if(nums[j]<nums[i]){
+            dp[i] = max(1 + func(j, nums), dp[i]);
         }
-        return dp[i] = ans;
+      }
+      return dp[i];
     }
-
     int lengthOfLIS(vector<int>& nums) {
-        int N = nums.size();
-        dp.assign(N, -1);
+        int n = nums.size();
+        dp.assign(n, -1);
         int ans = 0;
-        for (int i = 0; i < N; ++i) {
-            ans = max(ans, lis(i, nums));
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, func(i, nums));
         }
         return ans;
     }
