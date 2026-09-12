eb9456ceff2449455e9d9f8cc995f82ed1fffad2
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-09-12 16:53:14 +0530

    Time: 0 ms (100%), Space: 14.9 MB (44.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,28 +1,14 @@
-class Solution { 
-public: 
-    vector<int> twoSum(vector<int>& nums, int target) { 
-        int n = nums.size(); 
-        // sort(nums.begin(), nums.end()); 
-        vector<pair<int, int>> nums1; 
-        for(int i = 0; i < n; ++i) { 
-            nums1.push_back({nums[i], i}); 
-        } 
-        sort(nums1.begin(), nums1.end()); 
-        vector<int> ans; 
-        int l = 0, r = n - 1; 
-        while(l < r) { 
-            if(nums1[l].first + nums1[r].first > target) { 
-                r--; 
-            } 
-            else if(nums1[l].first + nums1[r].first < target) { 
-                l++; 
-            } 
-            else { 
-                ans.push_back(nums1[l].second); 
-                ans.push_back(nums1[r].second); 
-                break; 
-            } 
-        } 
-        return ans; 
-    } 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int needed = target - nums[i];
+            if (mp.find(needed) != mp.end()) {
+                return {mp[needed], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
 };
