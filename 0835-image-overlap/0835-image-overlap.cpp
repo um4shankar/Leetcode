8c45dd65cca442a8627de5849cb6402da54b067d
um4shankar <141544732+um4shankar@users.noreply.github.com>
2026-09-13 19:30:37 +0530

    Time: 62 ms (41.93%), Space: 16.1 MB (9.87%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
-
         vector<pair<int, int>> a, b;
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (img1[i][j] == 1)
@@ -19,22 +17,18 @@ public:
             swap(a, b);
 
         map<pair<int, int>, int> mp;
-
         for (auto p1 : a) {
             for (auto p2 : b) {
                 int dx = p2.first - p1.first;
                 int dy = p2.second - p1.second;
-
                 mp[{dx, dy}]++;
             }
         }
 
         int ans = 0;
-
         for (auto x : mp) {
             ans = max(ans, x.second);
         }
-
         return ans;
     }
 };
